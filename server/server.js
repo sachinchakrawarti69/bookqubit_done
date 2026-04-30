6a41000d5c3bd045414500603ec043ec46721020
Sachin Chakrawarti <chakrawartisachin012@gmail.com>
2026-04-30 15:35:50 +0530

    update

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Express Server Running 🚀");
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
