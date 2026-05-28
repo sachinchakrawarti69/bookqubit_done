c6a643b2b44b410ae465eed7755a006fea290e48
Sachin Chakrawarti <chakrawartisachin012@gmail.com>
2026-05-28 22:46:42 +0530

    up

--- web/src/app/layout.js
+++ web/src/app/layout.js
@@ -184,6 +184,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager - Main Script (placed high in <head>) */}
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PVP9RM4X');
+            `,
+          }}
+        />
+
         {/* Charset */}
         <meta charSet="utf-8" />
 
@@ -389,42 +404,17 @@ export default function RootLayout({ children }) {
       </head>
 
       <body className="antialiased">
-        {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-1SNY2TTZ4X"
-          strategy="afterInteractive"
-        />
+        {/* Google Tag Manager (noscript) - Immediately after opening <body> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PVP9RM4X"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
 
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-        >
-          {`
-            window.dataLayer = window.dataLayer || [];
-
-            function gtag() {
-              dataLayer.push(arguments);
-            }
-
-            gtag('js', new Date());
-
-            gtag('config', 'G-1SNY2TTZ4X', {
-              page_path: window.location.pathname,
-              anonymize_ip: true,
-              send_page_view: true,
-              allow_google_signals: true,
-              allow_anchor: true,
-            });
-
-            gtag('event', 'page_view', {
-              page_title: document.title,
-              page_location: window.location.href,
-              page_path: window.location.pathname,
-            });
-          `}
-        </Script>
-
-        {/* Pinterest Tag (Optional - for tracking) */}
+        {/* Pinterest Tag (Optional - Replace YOUR_PINTEREST_TAG_ID) */}
         <Script
           id="pinterest-tag"
           strategy="afterInteractive"
